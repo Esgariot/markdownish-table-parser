60c7f433aa5648b9a3c2b8e9cbb8dc3f0e907382
Esgariot <esgariot@icloud.com>
2021-09-25 13:05:36 +0200

    fix:  Add \\ escape

--- TableParser/Parsing2.fs
+++ TableParser/Parsing2.fs
@@ -16,23 +16,18 @@ module TableParsing =
 
 
     let pWall = pchar '|'
-    let pEscape = pipe2 (pchar '\\') (anyOf "|\n") (fun a b -> [a;b])
-    let pNonEscape = noneOf "\|\n"
+    let pEscape = pipe2 (pchar '\\') (anyOf "\\|\n") (fun a b -> [a;b])
+    let pNonEscape = noneOf "\\\|\n"
     let pCellChar = (pNonEscape |>> (fun x -> [x])) <|> pEscape
-    let pCell = many (pCellChar) |>> (List.concat >> List.toArray) |>> (fun x-> new string(x))
-    let pRow u = (sepEndBy1 pCell pWall) 
-                    // |>> (fun s -> s.[1..^1]) 
-                    >>= (fun cs -> cs |> (u << List.length) >>. preturn cs)
+    let pCell = many pCellChar |>> (List.concat >> List.toArray) |>> (fun x -> new string(x))
+    let pRow u = spaces >>. pWall >>. (sepBy1 pCell pWall) .>> skipRestOfLine false |>> (fun s -> s.[..^1]) 
+                >>= (fun cs -> cs |> (u << List.length) >>. preturn cs)
     let pTable = 
         let setCol cs = updateUserState (fun u -> { u with Columns = Some cs })
-        let checkCol cs =  userStateSatisfies (fun {Columns = c} -> cs = 0 || c |> exists ((=) cs) ) // TODO: Make it so 'cs = 0' can be dropped
+        let checkCol cs =  userStateSatisfies (fun {Columns = c} -> c |> exists ((=) cs) ) <?> "Matching column count"
         spaces >>. pRow setCol .>> spaces .>>. sepEndBy1 (pRow checkCol) spaces 
         |>> (fun (first, rest) -> first::rest) 
-        // |>> filter (not << List.isEmpty)
 
-
-   
-//exists((=)c))
 module DomainParsing =
     open FParsec
 
@@ -45,8 +40,8 @@ module Testing =
     let Table =
             """
             |Component   	|100    | 100       	|	101		| 100   |   101 |
-            |ColumnName  	|a      |  b        	| Parent	| Id    |   Id  |
-            |ColumnType  	|int    |string			| parent	| id    |   id  |
+            |ColumnName  	|a      |  b        	| Parent	| Id    |   \\  |
+            |ColumnType  	|int    |string			| parent	| id    |   \|  |
             |---------------|-------|---------------|-----------|-------|-------|
             |				| 10    | abba          |           | 1..10 |       |
             |               |       |               |     1     |       |   2   |
